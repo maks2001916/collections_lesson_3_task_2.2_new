deade0d17e133ea9d1d6612348684a21d815dbba
maks <mamonov.maksim.20003@gmail.com>
2022-10-27 21:10:36 +0500

    задание выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String storage = "Объект - ";
+        String storageOne = "";
+        int key = 0;
+        for (int i = 0; i < 10; i++) {
+            String storageTwo = String.valueOf(key);
+            storageOne = storage + storageTwo;
+            Storage storage0 = new Storage(key, storageOne);
+            System.out.println(storage0);
+            key++;
+        }
     }
 }
--- src/Storage.java
+++ src/Storage.java
@@ -1,2 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class Storage {
+    private Map<Integer, String> storage;
+    private int key;
+    private String meaning;
+
+    Map<Integer, String> storages = new HashMap<>();
+    public Storage(Integer key, String meaning) {
+        this.key = key;
+        this.meaning = meaning;
+        storages.put(this.key, this.meaning);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Storage storage1 = (Storage) o;
+        return key == storage1.key && Objects.equals(storage, storage1.storage) && Objects.equals(meaning, storage1.meaning) && Objects.equals(storages, storage1.storages);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(storage, key, meaning, storages);
+    }
+
+    @Override
+    public String toString() {
+        return "{ ключ - " + key + " " +
+                meaning  + '}';
+    }
 }
